5ea8543b8c5d6ad19402a225c7ddbc433b8e2d34
Shoaib Raza <shoaib_pki@hotmail.com>
2022-07-02 03:14:30 +0100

    update final version

--- src/com/servlet/AddProduct.java
+++ src/com/servlet/AddProduct.java
@@ -1,10 +1,22 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.ecommerce.EProduct;
+import com.util.HibernateUtil;
 
 /**
  * Servlet implementation class AddProduct
@@ -32,8 +44,27 @@ public class AddProduct extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		try {
+			SessionFactory sf = HibernateUtil.getSessionFactory();
+			
+			Session session = sf.openSession();
+			Transaction tx = session.beginTransaction();
+			
+			EProduct product = new EProduct();
+			
+			product.setName(request.getParameter("pname"));
+			product.setPrice(BigDecimal.valueOf(Double.valueOf((request.getParameter("price")))));
+			product.setDateAdded(new Date());
+			session.save(product);
+			tx.commit();
+			PrintWriter out = response.getWriter();
+			out.print("<h3>Data has been Saved!</h3>");
+			sf.close();
+			session.close();
+		}catch (Exception ex) {
+			throw ex;
+		}
 	}
+
 
 }
