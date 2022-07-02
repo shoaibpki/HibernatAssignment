f1e39737ab6a98ec3367ecb4015fa0412bc4b49b
Shoaib Raza <shoaib_pki@hotmail.com>
2022-07-02 01:56:27 +0100

    added hibernate sessionfactory class

---
+++ com/util/HibernateUtil.java
@@ -0,0 +1,25 @@
+package com.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory;
+	
+	static {
+		try {
+			StandardServiceRegistry ssr = 
+					new StandardServiceRegistryBuilder()
+					.configure("hibernate.cfg.xml").build();
+			Metadata metaData = new MetadataSources(ssr).getMetadataBuilder().build();
+			sessionFactory = metaData.getSessionFactoryBuilder().build();
+		} catch (Throwable th) {
+			throw new ExceptionInInitializerError(th);
+		}
+	}
+	
+
+}
